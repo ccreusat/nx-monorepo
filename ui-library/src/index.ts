62bcc6b90caa18fb610964592539e8139e4bb663
Clement Creusat <clement.creusat@gmail.com>
2024-03-25 18:55:25 +0100

    feat(ui): add component

---
+++ ui-library/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/counter/counter';
+export * from './lib/ui-library';
---
+++ ui-library/src/lib/ui-library.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UiLibraryProps {}
+
+export function UiLibrary(props: UiLibraryProps) {
+  return (
+    <div>
+      <h1>Welcome to UiLibrary!</h1>
+    </div>
+  );
+}
+
+export default UiLibrary;
