26543fd1adf7461fb7476709a60e6a6cdce3e967
Clement Creusat <clement.creusat@gmail.com>
2024-03-29 13:30:28 +0100

    fix(ui-library): add card story

--- libs/react/ui-library/src/lib/components/card/card.stories.tsx
+++ libs/react/ui-library/src/lib/components/card/card.stories.tsx
@@ -1,24 +1,70 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Card } from './card';
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Meta, StoryObj } from '@storybook/react';
 
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
+import Card from './card';
+import { useState } from 'react';
 
 const meta: Meta<typeof Card> = {
+  title: 'Components/Card/Base',
   component: Card,
-  title: 'Card',
+  args: {
+    isSelectable: true,
+    isSelected: false,
+  },
 };
+
 export default meta;
 type Story = StoryObj<typeof Card>;
 
-export const Primary = {
-  args: {},
+export const Base: Story = {
+  render: (args) => {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>Title</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  },
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/Welcome to Card!/gi)).toBeTruthy();
+export const IsSelectable: Story = {
+  render: (args) => {
+    const [selected, setSelected] = useState(false);
+    const handleOnClick = () => {
+      setSelected((prev) => !prev);
+    };
+    const handleOnSelect = () => {
+      setSelected((prev) => !prev);
+    };
+    return (
+      <Card
+        isSelectable={args.isSelectable}
+        isSelected={selected || args.isSelected}
+        onClick={handleOnClick}
+        onSelect={handleOnSelect}
+      >
+        <Card.Header />
+        <Card.Body>
+          <Card.Title>Title</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  },
+};
+
+export const SelectedState: Story = {
+  render: (args) => {
+    return (
+      <Card isSelected={args.isSelected}>
+        <Card.Header />
+        <Card.Body>
+          <Card.Title>Title</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  },
+  args: {
+    isSelected: true,
   },
 };
